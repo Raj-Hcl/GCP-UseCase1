720444e8b2f8ff287bdf9b5dca8a90829867e1d0
Raj-Hcl <raj.tripathi@hcltech.com>
2026-06-25 12:31:50 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,47 @@
+resource "google_compute_network" "vpc_network" {
+  name                    = "custom-vpc"
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "private-subnet"
+  ip_cidr_range = "10.0.1.0/27"
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_firewall" "ssh_rule" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "private-vm"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet.id
+  }
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    apt update
+    apt install -y apache2
+    systemctl start apache2
+    systemctl enable apache2
+    echo "Hello from Private VM Apache Server" > /var/www/html/index.html
+  EOT
+}
