b201c4bd3b60dbacbe711595c25e4513fcb0bf63
Raj-Hcl <raj.tripathi@hcltech.com>
2026-06-25 12:40:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "google_compute_network" "vpc_network" {
-  name                    = "custom-vpc"
+  name = "practice-vpc"
 }
 
 resource "google_compute_subnetwork" "subnet" {
-  name          = "private-subnet"
+  name          = "practice-subnet"
   ip_cidr_range = "10.0.1.0/27"
   region        = var.region
   network       = google_compute_network.vpc_network.id
